cd78322460098f0e09267d83c2d14d7a0db186ed
YohananDiamond <mitonanan12@gmail.com>
2020-05-19 16:15:12 -0300

    More things to Xresources to cover

--- dmenu.c
+++ dmenu.c
@@ -875,22 +875,56 @@ readxrdb(void)
 			fonts[0] = strdup(xval.addr);
 		else
 			fonts[0] = strdup(fonts[0]);
+
 		if (XrmGetResource(xdb, "dmenu.norm.bg", "*", &type, &xval))
 			colors[SchemeNorm][ColBg] = strdup(xval.addr);
 		else
 			colors[SchemeNorm][ColBg] = strdup(colors[SchemeNorm][ColBg]);
+
 		if (XrmGetResource(xdb, "dmenu.norm.fg", "*", &type, &xval))
 			colors[SchemeNorm][ColFg] = strdup(xval.addr);
 		else
 			colors[SchemeNorm][ColFg] = strdup(colors[SchemeNorm][ColFg]);
+
+		if (XrmGetResource(xdb, "dmenu.norm.hl.fg", "*", &type, &xval))
+			colors[SchemeNormHighlight][ColFg] = strdup(xval.addr);
+		else
+			colors[SchemeNormHighlight][ColFg] = strdup(colors[SchemeNormHighlight][ColFg]);
+
+		if (XrmGetResource(xdb, "dmenu.norm.hl.bg", "*", &type, &xval))
+			colors[SchemeNormHighlight][ColBg] = strdup(xval.addr);
+		else
+			colors[SchemeNormHighlight][ColBg] = strdup(colors[SchemeNormHighlight][ColBg]);
+
 		if (XrmGetResource(xdb, "dmenu.sel.bg", "*", &type, &xval))
 			colors[SchemeSel][ColBg] = strdup(xval.addr);
 		else
 			colors[SchemeSel][ColBg] = strdup(colors[SchemeSel][ColBg]);
+
 		if (XrmGetResource(xdb, "dmenu.sel.fg", "*", &type, &xval))
 			colors[SchemeSel][ColFg] = strdup(xval.addr);
 		else
 			colors[SchemeSel][ColFg] = strdup(colors[SchemeSel][ColFg]);
+
+		if (XrmGetResource(xdb, "dmenu.sel.hl.fg", "*", &type, &xval))
+			colors[SchemeSelHighlight][ColFg] = strdup(xval.addr);
+		else
+			colors[SchemeSelHighlight][ColFg] = strdup(colors[SchemeSelHighlight][ColFg]);
+
+		if (XrmGetResource(xdb, "dmenu.sel.hl.bg", "*", &type, &xval))
+			colors[SchemeSelHighlight][ColBg] = strdup(xval.addr);
+		else
+			colors[SchemeSelHighlight][ColBg] = strdup(colors[SchemeSelHighlight][ColBg]);
+
+		if (XrmGetResource(xdb, "dmenu.out.fg", "*", &type, &xval))
+			colors[SchemeOut][ColFg] = strdup(xval.addr);
+		else
+			colors[SchemeOut][ColFg] = strdup(colors[SchemeOut][ColFg]);
+
+		if (XrmGetResource(xdb, "dmenu.out.bg", "*", &type, &xval))
+			colors[SchemeOut][ColBg] = strdup(xval.addr);
+		else
+			colors[SchemeOut][ColBg] = strdup(colors[SchemeOut][ColBg]);
 
 		XrmDestroyDatabase(xdb);
 	}
